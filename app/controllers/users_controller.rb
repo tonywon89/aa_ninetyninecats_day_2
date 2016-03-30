78aea78460b0d8e0f71f89d531fc1d29340f818f
Tony Won <tonywon89@gmail.com>
2016-03-30 14:57:42 -0700

    finish user signup form and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to cats_url
+    else
+      flash[:error] = "Invalid combination"
+      redirect_to new_user_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :password)
   end
 end
